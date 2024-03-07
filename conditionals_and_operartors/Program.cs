f4404445179c0cc9dcd347107f899b96efc5fa8b
eveling13 <evelingjaramillo19@gmail.com>
2024-03-07 15:24:17 -0500

    add conditionals and operators

---
+++ conditionals_and_operartors/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Conditionals*/
+// Console.WriteLine("Ingrese un numero: ");
+// int num = int.Parse(Console.ReadLine());
+
+// if (num > 0) Console.WriteLine("Positivo");
+// else if (num < 0) Console.WriteLine("Negativo");
+// else Console.WriteLine("Es cero");
+/*End Conditionals*/
+
+
+/*Operators*/
+Console.WriteLine("Ingrese el primer numero: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
+Console.WriteLine($"Sumas: {a+b}, Resta: {a-b}, Multiplicacion: {a*b}, Division: {(b!=0 ? a/b : Indeterminado)}");
+/*End Operators*/
